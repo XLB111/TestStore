1b26855fece8a544b3c5bb8e1d03c26f0ef97d04
Robben <786790264@qq.com>
2020-06-23 14:06:41 +0800

    init

---
+++ Test/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// 测试
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Test/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Test
+{
+#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c=> {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API",
+                    Version = "v1",
+                    Description ="test"
+                });
+
+                //加载程序集的xml描述文档
+                var baseDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+                var xmlFile = System.AppDomain.CurrentDomain.FriendlyName + ".xml";
+                var xmlPath = Path.Combine(baseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath, true);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.DocumentTitle="Test";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","API v1");
+            });
+        }
+    }
+}
